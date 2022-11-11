b4f6270f6ee48c9dbf1ed00e75e2431f06835c82
WiFoDev <lou81804@gmail.com>
2022-11-10 21:22:46 -0500

    add connect wallet home button

--- src/components/Connected.tsx
+++ src/components/Connected.tsx
@@ -1,13 +1,23 @@
+import {useWalletModal} from "@solana/wallet-adapter-react-ui";
 import React from "react";
 
 export const Connected = () => {
+  const walletModal = useWalletModal();
+
+  const handleConnect = () => {
+    walletModal.setVisible(true);
+  };
+
   return (
     <>
       <div className="text-6xl font-semibold text-center">
         <h1>Mint your buildoor.</h1>
         <p className="text-white">Earn $BLD. Level up.</p>
       </div>
-      <button className="p-3 rounded w-60 bg-primary">
+      <button
+        className="p-3 rounded w-60 bg-primary"
+        onClick={handleConnect}
+      >
         become a buildoor
       </button>
     </>
