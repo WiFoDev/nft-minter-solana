73464cd0d3611ad799536a67f7453d452e5bb361
WiFoDev <lou81804@gmail.com>
2022-11-10 21:19:52 -0500

    add connected component

---
+++ src/components/Connected.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Connected = () => {
+  return (
+    <>
+      <div className="text-6xl font-semibold text-center">
+        <h1>Mint your buildoor.</h1>
+        <p className="text-white">Earn $BLD. Level up.</p>
+      </div>
+      <button className="p-3 rounded w-60 bg-primary">
+        become a buildoor
+      </button>
+    </>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import type {NextPage} from "next";
 
+import {Connected} from "@/components";
+
 const Home: NextPage = () => {
   return (
     <section className="flex flex-col items-center gap-10">
-      <div className="text-6xl font-semibold text-center">
-        <h1>Mint your buildoor.</h1>
-        <p className="text-white">Earn $BLD. Level up.</p>
-      </div>
-      <button className="p-3 rounded w-60 bg-primary">
-        become a buildoor
-      </button>
+      <Connected />
     </section>
   );
 };
